3576cf634e2965d87297665f59f4f855c77b9abc
LeoGes-bit <74633027+LeoGes-bit@users.noreply.github.com>
2021-02-23 02:23:17 -0500

    Reportes igual reportes de tablas

--- Controllers/PacientesController.cs
+++ Controllers/PacientesController.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -132,5 +135,56 @@ namespace WebAppDermatologia.Controllers
             }
             base.Dispose(disposing);
         }
+
+        public ActionResult RptPaciente()
+        {
+            return View();
+        }
+
+        public ActionResult DescargarReportePaciente()
+        {
+            try
+            {
+                var rptH = new ReportClass();
+                rptH.FileName = Server.MapPath("/Reporte/Pacientes.rpt");
+                rptH.Load();
+
+                //rptH.SetParameterValue("DptoId", codigo);
+                //rptH.SetParameterValue("ParamAlgo", algo);
+
+                // Report connection
+                var connInfo = Reporte.CrystalReportsCnn.GetConnectionInfo();
+                TableLogOnInfo logonInfo = new TableLogOnInfo();
+                Tables tables;
+                tables = rptH.Database.Tables;
+                foreach (Table table in tables)
+                {
+                    logonInfo = table.LogOnInfo;
+                    logonInfo.ConnectionInfo = connInfo;
+                    table.ApplyLogOnInfo(logonInfo);
+                }
+
+                Response.Buffer = false;
+                Response.ClearContent();
+                Response.ClearHeaders();
+
+                //En PDF
+                Stream stream = rptH.ExportToStream(ExportFormatType.PortableDocFormat);
+                rptH.Dispose();
+                rptH.Close();
+                return new FileStreamResult(stream, "application/pdf");
+
+                //En Excel
+                //Stream stream = rptH.ExportToStream(ExportFormatType.Excel);
+                //stream.Seek(0, SeekOrigin.Begin);
+                //return File(stream, "application/vnd.ms-excel", "empleadoRpt.xls");
+            }
+            catch (Exception )
+            {
+                throw;
+            }
+                
+            }
+        }
     }
-}
+
---
+++ Controllers/ReportesController.cs
@@ -0,0 +1,168 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebAppDermatologia.Models;
+
+namespace WebAppDermatologia.Controllers
+{
+    public class ReportesController : Controller
+    {
+        private DermatologiaEntities db = new DermatologiaEntities();
+
+        public ActionResult RptReserva()
+        {
+            return View();
+        }
+        public ActionResult RptEmpleados()
+        {
+            return View();
+        }
+        public ActionResult RptHistoriaClinica()
+        {
+            return View();
+        }
+        public ActionResult DescargarReporteReserva()
+        {
+            try
+            {
+                var rptH = new ReportClass();
+                rptH.FileName = Server.MapPath("/Reporte/reservas.rpt");
+                rptH.Load();
+
+                //rptH.SetParameterValue("DptoId", codigo);
+                //rptH.SetParameterValue("ParamAlgo", algo);
+
+                // Report connection
+                var connInfo = Reporte.CrystalReportsCnn.GetConnectionInfo();
+                TableLogOnInfo logonInfo = new TableLogOnInfo();
+                Tables tables;
+                tables = rptH.Database.Tables;
+                foreach (Table table in tables)
+                {
+                    logonInfo = table.LogOnInfo;
+                    logonInfo.ConnectionInfo = connInfo;
+                    table.ApplyLogOnInfo(logonInfo);
+                }
+
+                Response.Buffer = false;
+                Response.ClearContent();
+                Response.ClearHeaders();
+
+                //En PDF
+                Stream stream = rptH.ExportToStream(ExportFormatType.PortableDocFormat);
+                rptH.Dispose();
+                rptH.Close();
+                return new FileStreamResult(stream, "application/pdf");
+
+                //En Excel
+                //Stream stream = rptH.ExportToStream(ExportFormatType.Excel);
+                //stream.Seek(0, SeekOrigin.Begin);
+                //return File(stream, "application/vnd.ms-excel", "empleadoRpt.xls");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+        public ActionResult DescargarReporteEmpleado()
+        {
+            try
+            {
+                var rptH = new ReportClass();
+                rptH.FileName = Server.MapPath("/Reporte/Empleados.rpt");
+                rptH.Load();
+
+                //rptH.SetParameterValue("DptoId", codigo);
+                //rptH.SetParameterValue("ParamAlgo", algo);
+
+                // Report connection
+                var connInfo = Reporte.CrystalReportsCnn.GetConnectionInfo();
+                TableLogOnInfo logonInfo = new TableLogOnInfo();
+                Tables tables;
+                tables = rptH.Database.Tables;
+                foreach (Table table in tables)
+                {
+                    logonInfo = table.LogOnInfo;
+                    logonInfo.ConnectionInfo = connInfo;
+                    table.ApplyLogOnInfo(logonInfo);
+                }
+
+                Response.Buffer = false;
+                Response.ClearContent();
+                Response.ClearHeaders();
+
+                //En PDF
+                Stream stream = rptH.ExportToStream(ExportFormatType.PortableDocFormat);
+                rptH.Dispose();
+                rptH.Close();
+                return new FileStreamResult(stream, "application/pdf");
+
+                //En Excel
+                //Stream stream = rptH.ExportToStream(ExportFormatType.Excel);
+                //stream.Seek(0, SeekOrigin.Begin);
+                //return File(stream, "application/vnd.ms-excel", "empleadoRpt.xls");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+        public ActionResult DescargarReporteHistoriaClinica()
+        {
+            try
+            {
+                var rptH = new ReportClass();
+                rptH.FileName = Server.MapPath("/Reporte/HistoriaClinica.rpt");
+                rptH.Load();
+
+                //rptH.SetParameterValue("DptoId", codigo);
+                //rptH.SetParameterValue("ParamAlgo", algo);
+
+                // Report connection
+                var connInfo = Reporte.CrystalReportsCnn.GetConnectionInfo();
+                TableLogOnInfo logonInfo = new TableLogOnInfo();
+                Tables tables;
+                tables = rptH.Database.Tables;
+                foreach (Table table in tables)
+                {
+                    logonInfo = table.LogOnInfo;
+                    logonInfo.ConnectionInfo = connInfo;
+                    table.ApplyLogOnInfo(logonInfo);
+                }
+
+                Response.Buffer = false;
+                Response.ClearContent();
+                Response.ClearHeaders();
+
+                //En PDF
+                Stream stream = rptH.ExportToStream(ExportFormatType.PortableDocFormat);
+                rptH.Dispose();
+                rptH.Close();
+                return new FileStreamResult(stream, "application/pdf");
+
+                //En Excel
+                //Stream stream = rptH.ExportToStream(ExportFormatType.Excel);
+                //stream.Seek(0, SeekOrigin.Begin);
+                //return File(stream, "application/vnd.ms-excel", "empleadoRpt.xls");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+
+    }
+
+}
